8a9776579a28eb64ce610a6a399f5b4ecf49b69b
masaki39 <masa21gifus@gmal.com>
2024-04-14 21:52:02 +0900

    start development

--- main.ts
+++ main.ts
@@ -1,77 +1,30 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, TFolder } from 'obsidian';
+import {App, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile, TFolder } from 'obsidian';
+
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface SimpleCitationsSettings {
+	jsonPath: string;
+	folderPath: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: SimpleCitationsSettings = {
+	jsonPath: 'MyLibrary.json',
+	folderPath: "Literatures",
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class SimpleCitations extends Plugin {
+	settings: SimpleCitationsSettings;
+	
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'test',
-			name: 'test command',
+			id: 'update-citations',
+			name: 'Update Literature Notes',
 			callback: async () => {
 
-				// get json and exsistiing check
+				// get json and folder exsistiing check
 				const jsonFile = this.app.vault.getFileByPath("mylibrary.json");
 				const folder = this.app.vault.getAbstractFileByPath("Literatures");
 				if (!jsonFile || !(folder instanceof TFolder)) {
@@ -79,32 +32,70 @@ export default class MyPlugin extends Plugin {
 					return; 
 				}
 				
-				// parse json Data
+				// parse json Data and files
 				const jsonContents = await this.app.vault.cachedRead(jsonFile);
 				const jsonData = JSON.parse(jsonContents);
-				console.log(jsonData);
-
-				// get elements
-				let citekeys: string[] = []
+				const files = folder.children;
+				
+				// check json file
 				for (let i = 0; i < jsonData.length; i++) {
 					const citekey = jsonData[i]['citation-key'];
-					citekeys.push(citekey);
-					console.log(citekey);
+					const targetFileName = "@" + citekey + ".md";
+					let targetFile = files.find(file => file.name === targetFileName);
+
+					// if nonexisting, create file
+					if (!targetFile){
+						await this.app.vault.create(`Literatures/${targetFileName}`,"");
+						targetFile = await this.app.vault.getFileByPath(`Literatures/${targetFileName}`) as TFile;
+					}
+					// update frontmatter
+					if (targetFile && targetFile instanceof TFile) {
+						await this.updateFrontMatter(targetFile,jsonData[i]);
+						
+					}
+				}
+			}
+		});
+
+		this.addCommand({
+			id: 'add-citations',
+			name: 'Add Literature Notes',
+			callback: async () => {
+
+				// get json and folder exsistiing check
+				const jsonFile = this.app.vault.getFileByPath("mylibrary.json");
+				const folder = this.app.vault.getAbstractFileByPath("Literatures");
+				if (!jsonFile || !(folder instanceof TFolder)) {
+					new Notice('Something wrong with the settings.');
+					return; 
+				}
+				
+				// parse json Data and files
+				const jsonContents = await this.app.vault.cachedRead(jsonFile);
+				const jsonData = JSON.parse(jsonContents);
+				const files = folder.children;
+				
+				// check json file
+				for (let i = 0; i < jsonData.length; i++) {
+					const citekey = jsonData[i]['citation-key'];
+					const targetFileName = "@" + citekey + ".md";
+					let targetFile = files.find(file => file.name === targetFileName);
+
+					// if nonexisting, create file
+					if (!targetFile){
+						await this.app.vault.create(`Literatures/${targetFileName}`,"");
+						targetFile = await this.app.vault.getFileByPath(`Literatures/${targetFileName}`) as TFile;
+						if (targetFile && targetFile instanceof TFile){
+							await this.updateFrontMatter(targetFile,jsonData[i]);
+						}
+					}
 				}
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new SimpleCitationsSettingTab(this.app, this));
+		
 	}
 
 	onunload() {
@@ -118,28 +109,35 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async updateFrontMatter(targetFile: TFile, item: any) {
+		await this.app.fileManager.processFrontMatter(targetFile, (fm) => {
+			fm.aliases = item['title'];
+			fm.title = item['title'];
+			if (item['author'] && Array.isArray(item['author'])) {
+				let authorsSet = new Set<string>(); 
+				item['author'].forEach((author: { given?: string; family?: string; literal?: string }) => {
+					let authorName = author.literal || `${author.given} ${author.family}`;
+					authorsSet.add(authorName.trim());
+				});
+				let authorsList = Array.from(authorsSet);
+				fm.authors = authorsList;
+			}
+			if (item['issued'] && Array.isArray(item['issued']['date-parts']) && item['issued']['date-parts'][0] && !isNaN(item['issued']['date-parts'][0][0])) {
+				fm.year = Number(item['issued']['date-parts'][0][0]);
+			}
+			fm.journal = item['container-title'];
+			fm.doi = "https://doi.org/" + item['DOI'];
+			fm.zotero = "zotero://select/items/@" + item['id'];
+		});
+	}
+
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class SimpleCitationsSettingTab extends PluginSettingTab {
+	plugin: SimpleCitations;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: SimpleCitations) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -150,13 +148,21 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Set Json File Path')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter path')
+				.setValue(this.plugin.settings.jsonPath)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.jsonPath = value;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('Set Literature Note Folder Path')
+			.addText(text => text
+				.setPlaceholder('Enter path')
+				.setValue(this.plugin.settings.folderPath)
+				.onChange(async (value) => {
+					this.plugin.settings.folderPath = value;
 					await this.plugin.saveSettings();
 				}));
 	}
