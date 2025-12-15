d66f01e507cc21ebb51b5357be20d7482646b1db
masaki39 <masa21gifus@gmail.com>
2025-12-15 09:13:34 +0900

    feat: update abstract processing to trim whitespace and normalize spacing

--- src/utils/updateContent.ts
+++ src/utils/updateContent.ts
@@ -2,9 +2,10 @@ import { App, TFile } from "obsidian";
 import { replaceCommentTags } from "./replaceCommentTags";
 
 export async function updateContent(app: App, targetFile: TFile, template: string, abstract: string) {
+    const trimmedAbstract = abstract.split("\n").map(line => line.trim()).join("\n").replace(/\n{2,}/g, "\n");
     await app.vault.process(targetFile, (fileContent: string) => {
         fileContent = replaceCommentTags(fileContent, "<!-- START_TEMPLATE -->", "<!-- END_TEMPLATE -->", template);
-        fileContent = replaceCommentTags(fileContent, "<!-- START_ABSTRACT -->", "<!-- END_ABSTRACT -->", abstract);
+        fileContent = replaceCommentTags(fileContent, "<!-- START_ABSTRACT -->", "<!-- END_ABSTRACT -->", trimmedAbstract);
         return fileContent;
     });
 }
